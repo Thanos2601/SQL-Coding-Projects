d4550613597ae8fff90f41eaea45e088c4153beb
damsalem <drummingdan@gmail.com>
2018-11-06 11:38:10 -0800

    Finished creating tables for the library project

--- library_sql.sql
+++ library_sql.sql
@@ -12,7 +12,7 @@ SELECT * FROM library_branch;
 CREATE TABLE publisher (
 	PublisherName VARCHAR(30) PRIMARY KEY NOT NULL,
 	Address VARCHAR(50) NOT NULL,
-	Phone VARCHAR(15) NOT NULL,
+	Phone VARCHAR(15) NOT NULL
 )
 SELECT * FROM publisher;
 
@@ -27,7 +27,7 @@ SELECT * FROM books;
 
 CREATE TABLE book_authors(
 	BooksID INT FOREIGN KEY REFERENCES books(BooksID),
-	AuthorName VARCHAR(30)
+	AuthorName VARCHAR(30) NOT NULL
 )
 SELECT * FROM book_authors;
 
@@ -38,3 +38,22 @@ CREATE TABLE book_copies(
 	Number_Of_Copies INT NOT NULL
 )
 SELECT * FROM book_copies;
+
+
+CREATE TABLE borrower(
+	CardNo INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(20) NOT NULL,
+	Address VARCHAR(50) NOT NULL,
+	Phone VARCHAR(15) NOT NULL
+)
+SELECT * FROM borrower;
+
+
+CREATE TABLE book_loans (
+	BooksID INT FOREIGN KEY REFERENCES books(BooksID),
+	BranchID INT FOREIGN KEY REFERENCES library_branch(BranchID),
+	CardNo INT FOREIGN KEY REFERENCES borrower(CardNo),
+	DateOut DATE NOT NULL,
+	DateDue DATE NOT NULL
+)
+SELECT * FROM book_loans;

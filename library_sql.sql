da5ec8b8dfa72ed015dad07c1c80d131998a9fec
damsalem <drummingdan@gmail.com>
2018-11-06 11:21:28 -0800

    Created tables for library project

---
+++ library_sql.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE db_library
+
+
+CREATE TABLE library_branch (
+	BranchID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	BranchName VARCHAR(20) NOT NULL,
+	Address VARCHAR(50) NOT NULL
+)
+SELECT * FROM library_branch;
+
+
+CREATE TABLE publisher (
+	PublisherName VARCHAR(30) PRIMARY KEY NOT NULL,
+	Address VARCHAR(50) NOT NULL,
+	Phone VARCHAR(15) NOT NULL,
+)
+SELECT * FROM publisher;
+
+
+CREATE TABLE books (
+	BooksID INT PRIMARY KEY NOT NULL,
+	Title VARCHAR(50) NOT NULL,
+	PublisherName VARCHAR(30) FOREIGN KEY REFERENCES publisher(PublisherName)
+)
+SELECT * FROM books;
+
+
+CREATE TABLE book_authors(
+	BooksID INT FOREIGN KEY REFERENCES books(BooksID),
+	AuthorName VARCHAR(30)
+)
+SELECT * FROM book_authors;
+
+
+CREATE TABLE book_copies(
+	BooksID INT FOREIGN KEY REFERENCES books(BooksID),
+	BranchID INT FOREIGN KEY REFERENCES library_branch(BranchID),
+	Number_Of_Copies INT NOT NULL
+)
+SELECT * FROM book_copies;
